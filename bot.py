c4d7fb4440f05ae265ec118e3f470024f18eab48
Conor <conorhamilton@icloud.com>
2024-09-27 16:34:58 +0100

    further setup and testing! bot is working!

--- bot.py
+++ bot.py
@@ -3,10 +3,16 @@ from dotenv import load_dotenv
 import discord
 from discord.ext import commands
 
+
 load_dotenv()
 token = os.getenv("DISCORD_TOKEN")
 
-bot = commands.Bot(command_prefix="!")
+# intents
+intents = discord.Intents.default()
+intents.message_content = True  
+
+# specified intents
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 
 @bot.event
@@ -17,6 +23,11 @@ async def on_ready():
 @bot.command()
 async def hello(ctx):
     await ctx.send("Hello! I'm your new bot.")
+
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Pong! 🏓")
 
 
 bot.run(token)
